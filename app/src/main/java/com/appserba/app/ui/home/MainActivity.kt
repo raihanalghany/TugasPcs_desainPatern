e6f94332eac704ffd26c9a5f20912a266ab4a251
raihanalghany <raihanalghany6@gmail.com>
2020-12-12 21:08:30 +0700

    first commit

---
+++ app/src/main/java/com/appserba/app/ui/home/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.appserba.app.ui.home
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.appserba.app.R
+import com.appserba.app.databinding.ActivityMainBinding
+import com.appserba.app.ui.auth.AppserbaAuth
+import com.appserba.app.ui.auth.AuthActivity
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.buttonLogout.setOnClickListener {
+            AppserbaAuth.logout(this){
+                startActivity(Intent(this, AuthActivity::class.java))
+                finish()
+            }
+        }
+    }
+}
+
+
+
